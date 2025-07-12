1540c45c32c3bc32944544c606cc0376d5a2ec7d
goyallucky <luckygoyal221982@gmail.com>
2025-07-12 13:01:47 +0530

    committed

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,19 @@
 import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
-    try{
-        await mongoose.connect(process.env.MONGODB_URL)
-    } catch(error){
-        console.log(error)
-    }
+    // try{
+    //     await mongoose.connect(process.env.MONGODB_URL)
+    // } catch(error){
+    //     console.log(error)
+    // }
+
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((e) => {
+        console.log("Error connecting to MongoDB", e);
+    });
 }
 
 export default connectToDatabase; 
